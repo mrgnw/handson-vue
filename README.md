980ad52f869fd2b2cfe8c0b204cb2236629b3379
Morgan <morganfwilliams@gmail.com>
2018-10-21 10:23:53 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# handson-vue
-Created with CodeSandbox
+## Features
+- Templates
+  - with validation
+- output to csv / upload to server
+
+```
+templates
+ [
+  { name: 'templateName',
+    columns: [{name, validator, nullable, about, value, locked }]
+  }
+]
+```
---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="hot-preview">
+  <div>
+    Select your template.
+    <model-select :options="options"
+    v-model="item"
+    placeholder="select item">
+    </model-select>
+  </div>
+
+    <HotTable :settings="settings"></HotTable>
+  </div>
+</template>
+
+<script>
+  import { HotTable } from '@handsontable/vue';
+  import { ModelSelect } from 'vue-search-select'
+
+  import Vue from 'vue';
+
+  export default {
+    data: function() {
+      return {
+        settings: {
+          data: [
+            ['', '', '', '', '', ''],
+            ['', '', '', '', '', ''],
+            ['', '', '', '', '', ''],
+            
+          ],
+          colHeaders: [
+            'ID',
+            'Country',
+            'Code',
+            'Currency',
+            'Level',
+            'Units',
+            'Date',
+            'Change'
+          ],
+          rowHeaders: false,
+          fixedRowsTop: 1,
+          allowInsertRow: true,
+        },
+        options: [
+          { value: '1', text: 'aa' + ' - ' + '1' },
+          { value: '2', text: 'ab' + ' - ' + '2' },
+          { value: '3', text: 'bc' + ' - ' + '3' },
+          { value: '4', text: 'cd' + ' - ' + '4' },
+          { value: '5', text: 'de' + ' - ' + '5' }
+        ],
+        item: {
+          value: '',
+          text: ''
+        },
+      };
+    },
+    components: {
+      HotTable
+    }
+  }
+</script>
+
+<style src="../node_modules/handsontable/dist/handsontable.full.css"></style>
+<style>
+  #test-hot {
+    width: 600px;
+    height: 400px;
+    overflow: hidden;
+  }
+</style>
